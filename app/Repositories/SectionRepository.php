78488b12a4b801bd942b33d659d50d1b9cb90c2f
Ayumu Usuda <usuda@co-well.jp>
2017-10-25 21:49:26 +0900

    routesの編集、constファイルの追加

--- app/Repositories/SectionRepository.php
+++ app/Repositories/SectionRepository.php
@@ -2,6 +2,7 @@
 namespace App\Repositories;
 
 use App\Repositories\TWSRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Created by PhpStorm.
@@ -14,36 +15,76 @@ class SectionRepository
 {
 
     public function fixBanner() {
-        $fixBanner = [
+        $rows = [
 
         ];
-        return $fixBanner;
+        return $rows;
     }
 
-    public function normal() {
-        $normal = [
-
+    public function normal($goodsName, $typeName, $sectionName) {
+        $rows = [
+            'totalCount' => 10,
+            'limit' => 10,
+            'offset' => 0,
+            'page' => 1,
+            'hasNext' => true,
+            'rows' => [
+                [
+                    'dispStartDate'=> '2017-01-01',
+                    'dispEndDate'=> '2017-01-01',
+                    'saleStartDate'=> '2017-01-01',
+                    'rentalStartDate'=> '2017-01-01',
+                    'imageUrl'=> 'https://tsutaya.jp/image/a.jpg',
+                    'title' => 'ラ・ラ・ランド',
+                    'supplement' => 'エマ・ストーン', // アーティスト名、著者、機種等
+                    'code' => 'JAN_CODE',
+                    'urlCode' => 'url code',
+                    'rate' => 2
+                ],
+                [
+                    'dispStartDate'=> '2017-01-01',
+                    'dispEndDate'=> '2017-01-01',
+                    'saleStartDate'=> '2017-01-01',
+                    'rentalStartDate'=> '2017-01-01',
+                    'imageUrl'=> 'https://tsutaya.jp/image/a.jpg',
+                    'title' => 'ワイルド・スピード　ＩＣＥ　ＢＲＥＡＫ',
+                    'supplement' => 'ヴィン・ディーゼル', // アーティスト名、著者、機種等
+                    'code' => 'JAN_CODE',
+                    'urlCode' => 'url code',
+                    'rate' => 3
+                ],
+                [
+                    'dispStartDate'=> '2017-01-01',
+                    'dispEndDate'=> '2017-01-01',
+                    'saleStartDate'=> '2017-01-01',
+                    'rentalStartDate'=> '2017-01-01',
+                    'imageUrl'=> 'https://tsutaya.jp/image/a.jpg',
+                    'title' => '美女と野獣',
+                    'supplement' => 'エマ・ワトソン', // アーティスト名、著者、機種等
+                    'code' => 'JAN_CODE',
+                    'urlCode' => 'url code',
+                    'rate' => 4
+                ],
+            ]
         ];
-        return $normal;
+        return $rows;
     }
 
     public function banner($goodsName, $typeName, $sectionName) {
-        $banner = [
+        $rows = [
 
         ];
-        return $banner;
+        return $rows;
     }
 
-    public function ranking($goodsName, $typeName, $sectionName) {
+    public function ranking($genreCode) {
+        $genreMap = config('genre_map');
+        if (key_exists($genreCode, $genreMap)) {
+            $rankingConcentrationCd = $genreMap[$genreCode];
+        } else {
+            throw new NotFoundHttpException();
+        }
         $tws = new TWSRepository;
-        return $tws->ranking('D045')->get();
-    }
-
-    public function recommend() {
-        $recommend = [
-
-        ];
-        return $recommend;
-
+        return $tws->ranking($rankingConcentrationCd)->get();
     }
 }
