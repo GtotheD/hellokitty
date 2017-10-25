a903be0e03f197a2f9ce0b7051a13db96a9c6c42
Ayumu Usuda <usuda@co-well.jp>
2017-10-25 14:55:26 +0900

    migrationの変更し、テーブル名にtsを付加

--- database/migrations/2017_10_13_084448_create_ts_structures_table.php
+++ database/migrations/2017_10_13_084448_create_ts_structures_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStructuresTable extends Migration
+class CreateTsStructuresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateStructuresTable extends Migration
      */
     public function up()
     {
-        Schema::create('structures', function (Blueprint $table) {
+        Schema::create('ts_structures', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('sort');
             $table->unsignedTinyInteger('goods_type');
@@ -38,6 +38,6 @@ class CreateStructuresTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('structures');
+        Schema::dropIfExists('ts_structures');
     }
 }
--- database/migrations/2017_10_13_084506_create_ts_sections_table.php
+++ database/migrations/2017_10_13_084506_create_ts_sections_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSectionsTable extends Migration
+class CreateTsSectionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateSectionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sections', function (Blueprint $table) {
+        Schema::create('ts_sections', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('structure_id');
+            $table->unsignedInteger('ts_structure_id');
             $table->unsignedTinyInteger('code_type');
             $table->text('code', 255);
             $table->text('url_code', 255);
@@ -40,6 +40,6 @@ class CreateSectionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sections');
+        Schema::dropIfExists('ts_sections');
     }
 }
