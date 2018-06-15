116aff0645e26da86a0ba5dfce9b4c0ff59f2698
Ayumu Usuda <usuda@co-well.jp>
2018-06-15 19:55:40 +0900

    インデックスの追加

---
+++ database/migrations/2018_06_15_193603_add_index_ts_himo_keywords2.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexTsHimoKeywords2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ts_himo_keywords2', function (Blueprint $table) {
+            $table->string('roman_alphabet',255)->change();
+            $table->index('roman_alphabet');
+            $table->string('hiragana',255)->change();
+            $table->index('hiragana');
+            $table->string('katakana',255)->change();
+            $table->index('katakana');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ts_himo_keywords2', function (Blueprint $table) {
+            $table->text('roman_alphabet',255)->change();
+            $table->dropIndex('roman_alphabet');
+            $table->text('hiragana',255)->change();
+            $table->dropIndex('hiragana');
+            $table->text('katakana',255)->change();
+            $table->dropIndex('katakana');
+        });
+    }
+}
