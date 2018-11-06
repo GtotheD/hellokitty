d7172c95f19ae9fc77e10d2e29fa0e41d35c87f1
Ayumu Usuda <usuda@co-well.jp>
2018-11-06 14:15:44 +0900

    クラス名が被ってしまって一度に実行できなかった為修正

--- database/migrations/2018_11_04_211200_add_index_to_ts_himo_release_orders_month_and_tap_genre_id.php
+++ database/migrations/2018_11_04_211200_add_index_to_ts_himo_release_orders_month_and_tap_genre_id.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIndexToTsHimoReleaseOrders extends Migration
+class AddIndexToTsHimoReleaseOrdersMonthAndTapGenreId extends Migration
 {
     /**
      * Run the migrations.
