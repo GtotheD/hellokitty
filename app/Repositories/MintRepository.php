2951fef34af3dc8097243da967b5578a136bf231
l_tsukegawa <tomohiro.sukegawa@ccc.co.jp>
2020-02-13 10:41:21 +0900

    認証キーの追加・Sectionへの反映漏れ修正

---
+++ app/Repositories/MintRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Exception\ClientException;
+
+class MintRepository extends ApiRequesterRepository
+{
+
+    protected $sort;
+    protected $offset;
+    protected $limit;
+    protected $apiHost;
+    protected $apiNewHost;
+
+    const MINT_API = '/api/Cwar2010'; // 作品詳細用
+
+    public function __construct($sort = 'asc', $offset = 0, $limit = 10)
+    {
+        parent::__construct();
+        $this->apiHost = env('MINT_API_HOST');
+    }
+
+    public function authKey($tInternalNumber)
+    {
+        $userAgent = 'GuzzleHttp/6.2.0 curl/7.29.0 PHP/7.0.14';
+        $this->apiPath = $this->apiHost . self::MINT_API;
+        
+        $request = [
+            'AuthId' => env('AUTH_ID'),
+            'AuthPass' => env('AUTH_PASS'),
+            'SqlDiscNumber' => env('SQL_DISC_NUMBER'),
+            'RequestType' => env('REQUEST_TYPE'),
+            'SqlValueList' => ["'" . $tInternalNumber . "'"]
+        ];
+        $this->queryParams = json_encode($request);
+        return $this->postBody(true);
+    }
+}
