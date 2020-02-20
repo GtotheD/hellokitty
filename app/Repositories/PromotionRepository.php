f7c313c2ffffd11530503f675662f6d31fef4879
Tomohiro Sukegawa <l-tsukegawa@users.noreply.10.80.2.11>
2020-02-20 11:51:57 +0900

    Update PromotionRepository.php

--- app/Repositories/PromotionRepository.php
+++ app/Repositories/PromotionRepository.php
@@ -14,7 +14,34 @@ use Illuminate\Support\Carbon;
 
 class PromotionRepository extends BaseRepository
 {
-    const COMMONCAUTION = '固定文言';
+    const COMMONCAUTION = '・応募完了時にログインIDに登録されているTカード番号を取得させていただきます。<br>'.
+                          '・ログインIDに登録しているTカード番号と当該作品レンタル時にご提示いただいTカード番号が異なる場合は、抽選の対象外となります。<br>'.
+                          '&nbsp;&nbsp;&nbsp;ログインIDに登録されているTカード番号の確認は<a href=\'https://tsite.jp/tm/pc/accounts/STKIp0401001.do\'>こちら</a><br>'.
+                          '・当選した場合、Tポイントの付与はログインIDに登録しているTカード番号になります。<br>'.
+                          '・当選した場合、賞品の発送先はログインIDに登録されている住所（氏名宛）になります。<br>'.
+                          '&nbsp;&nbsp;&nbsp;ログインIDに登録されている住所・氏名の確認は<a href=\'https://tsite.jp/tm/pc/accounts/STKIp0401001.do\'>こちら</a><br>'.
+                          '・以下のいずれかに該当する場合、当選が無効となります。<br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;- 当選賞品が進呈される前にTカード番号の変更、ログインIDの削除などを行った場合<br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;- Tカードの有効期限切れや紛失等によりTカードが無効となった場合<br>'.
+                          '<br>'.
+                          '・今回のご入力を通じてお客様からご提供いただきました個人情報は、以下の企業が取得いたします。<br>'.
+                          '【株式会社TSUTAYA（以下TSUTAYAといいます）が主催するキャンペーンまたはTSUTAYAへのご意見・ご要望及びTポイントの付与】<br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;取得企業：TSUTAYA及びカルチュア・コンビニエンス・クラブ株式会社（以下CCCといいます）<br>'.
+                          '【カルチュア・エンタテインメント株式会社（以下CEといいます）が主催するキャンペーンまたはCE へのご意見・ご要望及びTポイントの付与】<br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;取得企業：CE及びCCC<br>'.
+                          '<br>'.
+                          '・今回ご入力いただいた個人情報は、ご意見・ご要望の収集、キャンペーン当選者へのご連絡及びTポイントの付与のみに利用いたします。ただし、個人情報と切り離して利用することを原則に、'.
+                          '個人を特定することのできない各種情報を、各種マーケティングデータを作成するために使用することがあります。<br>'.
+                          '<br>'.
+                          '・今回ご入力いただいた個人情報は、お客様のご了承をいただかない限り、第三者に提供することはありません。ただし、法令等により開示を求められた場合、'.
+                          '人の生命および身体または財産などの重大な利益を保護するために緊急を要する場合には、'.
+                          'お客様にお断りすることなく当該情報を情報開示することがあります。'.
+                          'また、お客様からのご依頼事項に対応するために必要な範囲で、事前に秘密保持契約を締結している当社の関連会社または業務委託先会社などに対して、当該情報を開示することがあります。<br>'.
+                          '<br>'.
+                          '・個人情報の取り扱いに関しては、個人情報保護方針もあわせてご覧ください。<br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;-CCCの個人情報保護方針は<a href=\'https://www.ccc.co.jp/customer_management/privacy/index.html\'>こちら</a><br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;-「TSUTAYAの個人情報保護方針」「TSUTAYAネットカンパニーの個人情報保護方針」は<a href=\'https://www.tsutaya-ltd.co.jp/privacy/index.html\'>こちら</a><br>'.
+                          '&nbsp;&nbsp;&nbsp;&nbsp;-CEの個人情報保護方針は<a href=\'https://www.culture-ent.co.jp/pdf/privacyStatement.pdf\'>こちら</a><br>';    
 
     public function __construct($sort = 'asc', $offset = 0, $limit = 10)
     {
