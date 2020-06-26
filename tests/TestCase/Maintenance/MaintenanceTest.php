ab34ccaf1c89abbb91ac4facb6b565586039aaba
Binh Nguyen Thanh <techvn2012@gmail.com>
2020-06-26 10:11:34 +0700

    Fix validate format time

--- tests/TestCase/Maintenance/MaintenanceTest.php
+++ tests/TestCase/Maintenance/MaintenanceTest.php
@@ -27,19 +27,51 @@ class MaintenanceTest extends \Laravel\Lumen\Testing\TestCase
         $this->assertEquals($data, $result);
     }
 
-    public function testValidateDate() {
+    /**
+     * This function use to test valid for datetime
+     * Valid format is YYYY-MM-DD HH:II:SS or YYYY-MM-DD
+     */
+    public function testValidateDate()
+    {
         $repo = new \App\Repositories\MaintenanceRepository();
 
-        $result = $repo->validateDate('2020-04-09');
+        /**
+         * Common true
+         */
+        // Just date
+        $result = $repo->validateDate('2020-01-01'); // date min
         $this->assertEquals(true, $result);
-        $result = $repo->validateDate('2020-04-09 12:45:16');
+        $result = $repo->validateDate('2020-06-15'); // date middle
+        $this->assertEquals(true, $result);
+        $result = $repo->validateDate('2020-12-31'); // date max
         $this->assertEquals(true, $result);
 
+        // Date and time
+        $result = $repo->validateDate('2020-01-01 00:00:00'); // time min
+        $this->assertEquals(true, $result);
+        $result = $repo->validateDate('2020-06-25 12:30:45'); // time min
+        $this->assertEquals(true, $result);
+        $result = $repo->validateDate('2020-12-31 23:59:59'); // time max
+        $this->assertEquals(true, $result);
+
+
+        /**
+         * Common fail
+         */
+        $result = $repo->validateDate('2020-01-00');
+        $this->assertEquals(false, $result);
+        $result = $repo->validateDate('2020-00-01');
+        $this->assertEquals(false, $result);
+        $result = $repo->validateDate('2020-12-32');
+        $this->assertEquals(false, $result);
+        $result = $repo->validateDate('2020-13-31');
+        $this->assertEquals(false, $result);
+        $result = $repo->validateDate('20-04-09');
+        $this->assertEquals(false, $result);
         $result = $repo->validateDate('2020-4-9');
         $this->assertEquals(false, $result);
-        $result = $repo->validateDate('2020-14-9');
+        $result = $repo->validateDate('2020-14-09');
         $this->assertEquals(false, $result);
-
         $result = $repo->validateDate('2020-04-09 12:5:16');
         $this->assertEquals(false, $result);
         $result = $repo->validateDate('2020-04-09 26:05:16');
